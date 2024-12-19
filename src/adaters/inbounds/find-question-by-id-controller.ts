371a252d1c193374f1c513b801659537d865adea
Antuneschacuma <antuneschacuma@gmail.com>
2024-12-19 21:29:25 +0100

    fix: adjust id validation

--- src/adaters/inbounds/find-question-by-id-controller.ts
+++ src/adaters/inbounds/find-question-by-id-controller.ts
@@ -3,7 +3,7 @@ import { IFindQuestionById } from "../../core/ports/in";
 export class FindQuestionByIdController {
     constructor(private readonly findQuestionById: IFindQuestionById) {}
     
-    async findById(id: string): Promise<Question> {
+    async findById({id}:{id :string}): Promise<Question> {
         if (!id) throw new Error("ID inválido");
         const question = await this.findQuestionById.execute(id);
         if (!question) throw new Error("Questão não encontrada");
