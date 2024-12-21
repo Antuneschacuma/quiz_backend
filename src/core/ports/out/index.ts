379f136b30fcd2afa7d47dad2a810d8638401b8c
Antuneschacuma <antuneschacuma@gmail.com>
2024-12-21 10:15:22 +0100

    feat: adjust imports file to recieve update question file

--- src/core/ports/out/index.ts
+++ src/core/ports/out/index.ts
@@ -1,2 +1,4 @@
 export * from './icreate-question-repository';
 export * from './find-question-repository';
+export * from './find-all-question-repository';
+export * from './update-question-repository';
