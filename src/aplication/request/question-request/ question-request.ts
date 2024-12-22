6095c09f57f8387ff081a00c4fd7be3370bedb5f
Antuneschacuma <antuneschacuma@gmail.com>
2024-12-22 10:14:40 +0100

    adjust return to use of id in constructor

--- src/aplication/request/question-request/ question-request.ts
+++ src/aplication/request/question-request/ question-request.ts
@@ -1,7 +1,6 @@
 import { Difficulty } from "../../../core/enums";
 
 export class QuestionRequest {
-    //id?:string;
     content: string;
     options: string[];
     correctOption: string;
@@ -9,19 +8,26 @@ export class QuestionRequest {
     difficulty: Difficulty;
   
     constructor(
-      //id:string,
       content: string,
       options: string[],
       correctOption: string,
       category: string,
       difficulty: Difficulty
     ) {
-      //this.id=id;
       this.content = content;
       this.options = options;
       this.correctOption = correctOption;
       this.category = category;
       this.difficulty = difficulty;
     }
-  }
-  
+
+    setContent(content: string): void {this.content = content;}
+
+    setOptions(options: string[]): void {this.options = options;}
+
+    setCorrectOption(correctOption: string): void {this.correctOption = correctOption;}
+
+    setCategory(category: string): void {this.category = category;}
+
+    setDifficulty(difficulty: Difficulty): void {this.difficulty = difficulty;}
+}
